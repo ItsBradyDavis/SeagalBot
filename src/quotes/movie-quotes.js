eff6426f5ea4306ec8f75974543c3bce4ca56b17
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-03-03 20:35:40 -0600

    Adding padding & removing space

--- src/quotes/movie-quotes.js
+++ src/quotes/movie-quotes.js
@@ -5,7 +5,7 @@ const quotes = [
 	'I\'d like to make something very clear. I don\'t have rage. I\'m a happy guy. You see this face? This is a happy face. You all would be lucky to be as happy as I am!',
 	'Women. You can\'t trust em.',
 	'What kind of damn fool do you think I am? I\'m still in prison for doing the same thing you\'re about to ask me to do again.',
-	'Are you really that good? \n _Every once in a while._',
+	'Are you really that good? \n\n_Every once in a while._',
 	'*sits*',
 	'You gotta live to talk about old times.',
 ];
