5a8ccecc29b19617d81dcfd2d07fc4783fb612fe
Brady Davis <bradydavis.ver.2.0@gmail.com>
2022-03-08 17:54:13 -0600

    Updating map behavior

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,17 +1,17 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { clientId, guildId, token } from '../config.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { IRL_QUOTE, MOVIE_QUOTE } from './enums/slash-command-names.js';
 
-const commands = [
-	new SlashCommandBuilder().setName(IRL_QUOTE).setDescription('Replies with a real-life Seagal Quote!'),
-	new SlashCommandBuilder().setName(MOVIE_QUOTE).setDescription('Replies with a Seagal Movie Quote'),
-]
-	.map(command => command.toJSON());
+import { clientId, guildId, token } from '../config.js';
+import commandMap from './enums/command-map.js';
+
+const commands = Object.keys(commandMap).map((key) => (
+	new SlashCommandBuilder().setName(key).setDescription(commandMap[key].description))
+).map((command) => command.toJSON());
+
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
